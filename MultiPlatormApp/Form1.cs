f7ff8266d134d57cab0889b09a478aa27d882725
Seb <sebastien@yoctopuce.com>
2019-02-11 17:40:35 +0100

    first commit

---
+++ MultiPlatormApp/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiPlatormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "N/A";
+            label2.Text = "No sensor detected";
+            timer1.Interval = 100;
+            timer1.Tick += timer1_Tick;
+            timer1.Enabled = true;
+
+        }
+
+        YSensor sensor = null;
+        int hardwaredetect = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string errmsg = "";
+            if (hardwaredetect == 0)
+                YAPI.UpdateDeviceList(ref errmsg);
+            hardwaredetect = (hardwaredetect + 1) % 20;
+            YAPI.HandleEvents(ref errmsg);
+            if (sensor == null)
+                sensor = YSensor.FirstSensor();
+            if (sensor != null) {
+                if (sensor.isOnline()) {
+                    label1.Text = sensor.get_friendlyName();
+                    label2.Text = sensor.get_currentValue() + sensor.get_unit();
+                } else {
+                    label1.Text = "OFFLINE";
+                    label2.Text = "Sensor is offline";
+                    sensor = null;
+                }
+            }
+        }
+    }
+}
---
+++ MultiPlatormApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiPlatormApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            string errsmg = "";
+            if (YAPI.RegisterHub("usb", ref errsmg) == YAPI.SUCCESS)
+                Application.Run(new Form1());
+            else
+                MessageBox.Show("Init error:" + errsmg);
+
+            Application.Run(new Form1());
+        }
+    }
+}
